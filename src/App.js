b9a84d45e96c269eba1b0f2c7e5fcd9477968604
MayuriUdage <156458642+MayuriUdage@users.noreply.github.com>
2024-10-22 12:06:41 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    <div>
+      <button>Add</button>
+    </div>
+    <div>
+      <span>Name</span><br/>
+      <span>Phone</span>
+    </div>
+    </div>
+  );
+}
+
+export default App;
